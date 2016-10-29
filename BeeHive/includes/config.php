89768ebf83fbd142b70f11991f08b6f91484820f
root <root@ip-172-31-60-36.ec2.internal>
2016-10-29 06:33:38 +0000

    Auth Update

---
+++ BeeHive/includes/config.php
@@ -0,0 +1,9 @@
+<?php
+	define('DB_SERVER', '127.0.0.1:3306');
+	define('DB_USERNAME', 'root');
+	define('DB_PASSWORD', 'team8');
+	define('DB_DATABASE', 'Beehive');
+	
+	$dbc = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+?>
+	
