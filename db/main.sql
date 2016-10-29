404a9ac515c7bc104bdc6aeceae4d5f58267fde7
CBadame <carson.badame@gmail.com>
2016-10-28 22:02:28 -0400

    Database Created

--- db/main.sql
+++ db/main.sql
@@ -1,1 +1,37 @@
---TABLES FOR DATABASE
+CREATE DATABASE Beehive;
+
+CREATE TABLE Videos (
+	video_id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR (255) NOT NULL,
+	file VARCHAR (255) NOT NULL,
+	email VARCHAR (255) NOT NULL,
+	vid_length TIME,
+	explicit BOOLEAN,
+	upload_time TIME,
+	description VARCHAR (255),
+	PRIMARY KEY (video_id)
+);
+
+CREATE TABLE Users (
+	email VARCHAR (255) NOT NULL,
+	password VARCHAR (255) NOT NULL,
+	name VARCHAR (255),
+	PRIMARY KEY (email)
+);
+
+CREATE TABLE Tags (
+	tag_id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR (255) NOT NULL,
+	subdivision VARCHAR (255),
+	PRIMARY KEY (tag_id)
+);
+
+CREATE TABLE Video_Tag_Detail (
+	tag_id INT NOT NULL,
+	video_id INT NOT NULL,
+	CONSTRAINT pk_Video_Tag_Detail PRIMARY KEY (tag_id, video_id)
+);
+
+ALTER TABLE Videos ADD CONSTRAINT fk_email FOREIGN KEY (email) REFERENCES Users(email);
+ALTER TABLE Video_Tag_Detail ADD CONSTRAINT fk_video_id FOREIGN KEY (video_id) REFERENCES Videos(video_id);
+ALTER TABLE Video_Tag_Detail ADD CONSTRAINT fk_tag_id FOREIGN KEY (tag_id) REFERENCES Tags(tag_id);
